2221317b369090b51c396356c425e36f34270e13
Natalia Santos Martínez <nsanmar@upv.edu.es>
2023-06-26 13:37:13 +0200

    Distancia Usuario y Libro (coordenadas con latitud y longitud)

--- BiblioConnect/Scripts/API_mapa.js
+++ BiblioConnect/Scripts/API_mapa.js
@@ -1,26 +1,47 @@
 
+//Coordenadas de biblioteca
 function getLatLong(Calle, Ciudad, Estado, Pais, CodPostal) {
-    $.ajax({
-        url: "https://nominatim.openstreetmap.org/search.php",
-        data: {
-            street: Calle,
-            city: Ciudad,
-            state: Estado,
-            country: Pais,
-            postalcode: CodPostal,
-            format: "jsonv2"
-        },
-        type: "GET",
-        success: function (data) {
-            var latitud = data[0].lat;
-            var longitud = data[0].lon;
-            mostrarMapa(latitud, longitud); 
-        },
-        error: function (xhr, status, error) {
-            console.log("Error:", error);
-        }
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url: "https://nominatim.openstreetmap.org/search.php",
+            data: {
+                street: Calle,
+                city: Ciudad,
+                state: Estado,
+                country: Pais,
+                postalcode: CodPostal,
+                format: "jsonv2"
+            },
+            type: "GET",
+            success: function (data) {
+                console.log(data);
+                var latitud = data[0].lat;
+                var longitud = data[0].lon;
+                resolve([latitud, longitud]);
+            },
+            error: function (xhr, status, error) {
+                console.log("Error:", error);
+                reject(error);
+            }
+        });
     });
 }
+
+//Coordenadas de Usuario
+function getUserCurrentPosition() {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitud = position.coords.latitude;
+            var longitud = position.coords.longitude;
+            resolve([latitud, longitud]);
+        }, function (error) {
+            console.log("Error al obtener la posición del usuario:", error);
+            reject(error);
+        });
+    });
+}
+
+//LEAFLET
 function mostrarMapa(latitud, longitud) {
     var map = L.map('map').setView([latitud, longitud], 17);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -31,4 +52,26 @@ function mostrarMapa(latitud, longitud) {
     L.marker([latitud, longitud]).addTo(map)
         /*.bindPopup('A pretty CSS popup.<br> Easily customizable.')*/
         .openPopup();
+}
+
+//CÁLCULO DE LA DISTANCIA (fórmula de Haversine)
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const earthRadiusKm = 6371.0;
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(lat1)) *
+        Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadiusKm * c;
+    console.log(distance)
+    return distance;
+}
+
+function toRadians(degrees) {
+    return (degrees * Math.PI) / 180;
 }

00dc6de6623bed6d47152c02acd636341e772545
Natalia Santos Martínez <nsanmar@upv.edu.es>
2023-07-11 10:05:01 +0200

    REGISTRO LECTOR INTERFAZ

--- BiblioConnect/Validate/Registro.js
+++ BiblioConnect/Validate/Registro.js
@@ -154,6 +154,22 @@ document.addEventListener("DOMContentLoaded", function () {
             return true;
         }
     }
+
+    //Hide and show password
+    const eyeIcons = document.querySelectorAll(".show-hide");
+
+    eyeIcons.forEach((eyeIcon) => {
+        eyeIcon.addEventListener("click", () => {
+            const pInput = eyeIcon.parentElement.querySelector("input");
+            if (pInput.type === "password") {
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return pInput.type = "text"; 
+            }
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+            return pInput.type = "password";
+        });
+    }); 
+
     // Evento de envío del formulario
     form.addEventListener("submit", function (e) {
         e.preventDefault();
